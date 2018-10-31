cd40f83acccef6f46618c9938c4e12b0d622c8e3
chanonpingpong <chanon.pingpong@gmail.com>
2018-10-31 12:42:07 +0700

    temp close ws for production

--- src/utils/apollo-connector.js
+++ src/utils/apollo-connector.js
@@ -1,24 +1,25 @@
 import { ApolloClient } from 'apollo-client'
-import { split } from 'apollo-link'
+// import { split } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
-import { WebSocketLink } from 'apollo-link-ws'
-import { getMainDefinition } from 'apollo-utilities'
+// import { WebSocketLink } from 'apollo-link-ws'
+// import { getMainDefinition } from 'apollo-utilities'
 import Cookies from 'js-cookie'
 import fetch from 'node-fetch'
+// import ws from 'ws'
 
 const httpLink = createHttpLink({
   fetch,
   uri: `${process.env.API_URL}`,
 })
 
-const wsLink = new WebSocketLink({
-  uri: `${process.env.SUBSCRIPTION_URL}`,
-  options: {
-    reconnect: true,
-  },
-})
+// const wsLink = new WebSocketLink({
+//   uri: `${process.env.SUBSCRIPTION_URL}`,
+//   options: {
+//     reconnect: true,
+//   },
+// })
 
 const authLink = setContext((_, { headers }) => {
   const token = Cookies.get(process.env.AUTH_TOKEN_NAME)
@@ -30,17 +31,17 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === 'OperationDefinition' && operation === 'subscription'
-  },
-  wsLink,
-  authLink.concat(httpLink)
-)
+// const link = split(
+//   ({ query }) => {
+//     const { kind, operation } = getMainDefinition(query)
+//     return kind === 'OperationDefinition' && operation === 'subscription'
+//   },
+//   wsLink,
+//   authLink.concat(httpLink)
+// )
 
 const client = new ApolloClient({
-  link,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 

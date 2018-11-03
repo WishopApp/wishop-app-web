2b2cc06cb994a56ae283631221e2295a8e89f9da
chanonpingpong <chanon.pingpong@gmail.com>
2018-11-03 19:20:24 +0700

    add websocket for apollo connector

--- src/utils/apollo-connector.js
+++ src/utils/apollo-connector.js
@@ -1,25 +1,17 @@
 import { ApolloClient } from 'apollo-client'
-// import { split } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
-// import { WebSocketLink } from 'apollo-link-ws'
-// import { getMainDefinition } from 'apollo-utilities'
 import Cookies from 'js-cookie'
 import fetch from 'node-fetch'
-// import ws from 'ws'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = createHttpLink({
   fetch,
-  uri: `${process.env.API_URL}`,
+  uri: process.env.API_URL,
 })
-
-// const wsLink = new WebSocketLink({
-//   uri: `${process.env.SUBSCRIPTION_URL}`,
-//   options: {
-//     reconnect: true,
-//   },
-// })
 
 const authLink = setContext((_, { headers }) => {
   const token = Cookies.get(process.env.AUTH_TOKEN_NAME)
@@ -31,17 +23,28 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-// const link = split(
-//   ({ query }) => {
-//     const { kind, operation } = getMainDefinition(query)
-//     return kind === 'OperationDefinition' && operation === 'subscription'
-//   },
-//   wsLink,
-//   authLink.concat(httpLink)
-// )
+const wsLink = process.browser
+  ? new WebSocketLink({
+      uri: process.env.SUBSCRIPTION_URL,
+      options: {
+        reconnect: true,
+      },
+    })
+  : null
+
+const link = process.browser
+  ? split(
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' && operation === 'subscription'
+      },
+      wsLink,
+      authLink.concat(httpLink)
+    )
+  : httpLink
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 })
 

24af24d2f0f20cd5e48ae8b7c986cbb49614ca25
chanonpingpong <chanon.pingpong@gmail.com>
2018-11-15 17:38:36 +0700

    fix logout not working properly

--- src/containers/Logout.js
+++ src/containers/Logout.js
@@ -5,7 +5,7 @@ import Cookies from 'js-cookie'
 class Logout extends Component {
   componentWillMount() {
     Cookies.remove(process.env.AUTH_TOKEN_NAME)
-    this.props.history.push('/login')
+    window.location.href = '/login'
   }
 
   render() {
